46e4f16d262b53853f818ce0fc05b3f53e320d62
Kristian Calhoun <kristian.calhoun@gmail.com>
2013-02-27 14:30:42 -0500

    Added GUI components to View Match and View Team pages.

--- src/frcscout/WebContent/js/layout/indexLayout.js
+++ src/frcscout/WebContent/js/layout/indexLayout.js
@@ -8,21 +8,23 @@ function getContentItems(){
             items: getOverviewItems()
         },{
             title: 'View Match',
-            html: '<p> tab2 </p>'
+            html: '<p> tab2 </p>',
+            items: getViewMatchItems()
         },{
             title: 'View Team',
-            html: '<p> tab3 </p>'
+            html: '<p> tab3 </p>',
+            items: getViewTeamItems()
         }]
     }];
 }
 
 function getOverviewStore() {	// Temp data generator
     store1 = Ext.create('Ext.data.JsonStore', {
-        fields: ['team_number', 'total_points', 'autonomous', 'teleop', 'climb'],
-        sorters: ['team_number', 'total_points'],
+        fields: ['id', 'total_points', 'autonomous', 'teleop', 'climb'],
+        sorters: ['id', 'total_points'],
 	    data: [
-	           {team_number: 25, total_points: 100, autonomous: 40, teleop: 30, climb: 30},
-		       {team_number: 103, total_points: 70, autonomous: 36, teleop: 24, climb: 10}
+	           {id: 25, total_points: 100, autonomous: 40, teleop: 30, climb: 30},
+		       {id: 103, total_points: 70, autonomous: 36, teleop: 24, climb: 10}
 		       ]
     });
     return store1;
@@ -44,7 +46,7 @@ function getOverviewItems() {
     	}, {
     		type: 'Category',
     		position: 'left',
-    		fields: ['team_number'],
+    		fields: ['id'],
     		title: false
     	}],
     	series: [{
@@ -70,7 +72,7 @@ function getOverviewItems() {
 	return [{
         layout: {
         	type: 'hbox',
-        	align: 'stretch'
+        	align: 'left'
         },
         items: [{
         	xtype: 'gridpanel',
@@ -78,7 +80,7 @@ function getOverviewItems() {
             id: 'eventOverviewGrid',
             store: getOverviewStore(),
             columns: [
-                { header: 'Team #',  dataIndex: 'team_number', flex: 1  },
+                { header: 'Team #',  dataIndex: 'id', flex: 1},
                 { header: 'Total Points', dataIndex: 'total_points'},
                 { header: 'Auton. Points', dataIndex: 'autonomous' },
                 { header: 'Disc Points', dataIndex: 'teleop'},
@@ -87,5 +89,217 @@ function getOverviewItems() {
             height: 400,
             width: 600
         }, overviewChart]
+	}];
+}
+
+function getViewMatchItems() {
+	return [{
+		layout: {
+			type: 'vbox',
+			align: 'left',
+		},
+		items: [{
+			layout: {
+				type: 'hbox'
+			},
+			items: [{
+				xtype: 'panel',
+		        html: '<h1>Match [#]</h1>',
+			}, {
+				xtype: 'panel',
+				items: [{
+					xtype: 'textfield',
+					name: 'matchSearchBox',
+					fieldLabel: 'Match search'
+				}]
+			}, {
+				xtype: 'button',
+				text: 'Search',
+				handler: function() {
+					alert('Search for a specified match number.');
+				}
+			}]
+		}, {
+			xtype: 'gridpanel',
+			title: 'Red Alliance',
+			id: 'redAllianceGrid',
+			//store: getRedAllianceStore(),
+			columns: [
+			    { header: 'Team #',  dataIndex: 'id', flex: 1},
+                { header: 'Auton. Points', dataIndex: 'autonomous' },
+                { header: 'Disc Points', dataIndex: 'teleop'},
+                { header: 'Climb Points', dataIndex: 'climb'},
+                { header: 'Total Points', dataIndex: 'total_points'}
+			],
+			width: 500
+		}, {
+			xtype: 'gridpanel',
+			title: 'Blue Alliance',
+			id: 'blueAllianceGrid',
+			//store: getBlueAllianceStore(),
+			columns: [
+			    { header: 'Team #',  dataIndex: 'id', flex: 1},
+                { header: 'Auton. Points', dataIndex: 'autonomous' },
+                { header: 'Disc Points', dataIndex: 'teleop'},
+                { header: 'Climb Points', dataIndex: 'climb'},
+                { header: 'Total Points', dataIndex: 'total_points'}
+			],
+			width: 500
+		}, {
+			xtype: 'button',
+			text: 'View Match Record',
+			handler: function() {
+				alert('This button brings up a detailed view for the selected match record.');
+			}
+		}]
+	}];
+}
+
+function getTeamMatchStore() {	// Temp data generator
+    store1 = Ext.create('Ext.data.JsonStore', {
+        fields: ['id', 'total_points', 'autonomous', 'teleop', 'climb'],
+	    data: [
+	           {id: 1, total_points: 100, autonomous: 40, teleop: 33, climb: 30},
+		       {id: 5, total_points: 54, autonomous: 20, teleop: 24, climb: 10},
+		       {id: 7, total_points: 70, autonomous: 36, teleop: 24, climb: 10},
+		       {id: 12, total_points: 66, autonomous: 18, teleop: 28, climb: 20}
+		       ]
+    });
+    return store1;
+}
+
+function getTeamPieChartStore() {	// Temp data generator
+    store1 = getTeamMatchStore();
+    var pointTotals = {
+    		autonomous: 0,
+    		teleop: 0,
+    		climb: 0
+    };
+    for (var i = 0; i < store1.count(); i++) {
+    	pointTotals.autonomous += store1.getAt(i).autonomous;
+    	pointTotals.teleop += store1.getAt(i).teleop;
+    	pointTotals.climb += store1.getAt(i).climb;
+    }
+	var store2 = Ext.create('Ext.data.JsonStore', {
+    	fields: ['category', 'total'],
+    	data: [
+    	       { category: 'autonomous', total: 10 },
+    	       { category: 'teleop', total: 24 },
+    	       { category: 'climb', total: 10 }
+    	       ]
+    });
+    return store2;
+}
+
+function getViewTeamItems() {
+	return [{
+		layout: {
+			type: 'vbox',
+			align: 'left'
+		},
+		items: [{
+			layout: {
+				type: 'hbox'
+			},
+			items: [{
+				xtype: 'panel',
+				html: '<h1>Team [#]</h1>'
+			}, {
+				xtype: 'panel',
+				items: [{
+					xtype: 'textfield',
+					name: 'teamSearchBox',
+					fieldLabel: 'Team Search'
+				}]
+			}, {
+				xtype: 'button',
+				text: 'search',
+				handler: function() {
+					alert('This will search for matches and update the tables below!');
+				}
+			}]
+		}, {
+			xtype: 'gridpanel',
+			title: 'Match Records',
+			id: 'teamMatchGrid',
+			store: getTeamMatchStore(),
+			columns: [
+			    { header: 'Match #',  dataIndex: 'id', flex: 1},
+                { header: 'Auton. Points', dataIndex: 'autonomous' },
+                { header: 'Disc Points', dataIndex: 'teleop'},
+                { header: 'Climb Points', dataIndex: 'climb'},
+                { header: 'Total Points', dataIndex: 'total_points'}
+			],
+			width: 500
+		}, {
+			xtype: 'button',
+			text: 'View Match Record',
+			handler: function() {
+				alert('This will bring up a detailed view for the selected match record.');
+			}
+		}, {
+			layout: {
+				type: 'hbox'
+			}, 
+			items: [{
+				xtype: 'chart',
+				animate: true,
+				width: 300,
+		    	height: 300,
+		    	store: getTeamMatchStore(),
+		    	legend: {
+		    		position: 'left'
+		    	},
+		    	axes: [{
+		    		type: 'Numeric',
+		    		position: 'left',
+		    		grid: true,
+		    		minimum: 0
+		    	}, {
+		    		type: 'Category',
+		    		position: 'bottom',
+		    		fields: ['id'],
+		    		title: 'Match #'
+		    	}],
+		    	series: [{
+		    		type: 'line',
+		    		axis: 'left',
+		    		xField: 'id',
+		    		yField: 'total_points',
+		    		fill: true
+		    	}, {
+		    		type: 'line',
+		    		axis: 'left',
+		    		xField: 'id',
+		    		yField: 'autonomous'
+		    	}, {
+		    		type: 'line',
+		    		axis: 'left',
+		    		xField: 'id',
+		    		yField: 'teleop'
+		    	}, {
+		    		type: 'line',
+		    		axis: 'left',
+		    		xField: 'id',
+		    		yField: 'climb'
+		    	}]
+			}, {
+				xtype: 'chart',
+				animate: true,
+				width: 300,
+				height: 300,
+				store: getTeamPieChartStore(),
+				series: [{
+					type: 'pie',
+					angleField: 'total',
+					label: {
+						field: 'category',
+						display: 'inside',
+						contrast: true,
+						font: '12px Helvetica'
+					}
+				}]
+			}]
+		}]
 	}];
 }
