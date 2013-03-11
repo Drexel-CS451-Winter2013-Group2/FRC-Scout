17c5271ef4a4d616e3c1f84472d3cb00d846b659
Kristian Calhoun <kristian.calhoun@gmail.com>
2013-03-10 22:44:53 -0400

    Implement GroupByEventBean.getOverviewTable().

--- src/frcscout/src/com/frcscout/teammember/GroupByEventBean.java
+++ src/frcscout/src/com/frcscout/teammember/GroupByEventBean.java
@@ -1,6 +1,16 @@
 package com.frcscout.teammember;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import com.frcscout.sql.DBConnection;
 import com.frcscout.sql.MySQLConnection;
@@ -16,9 +26,40 @@ public class GroupByEventBean {
         selectedEvent = null;
     }
     
+    @SuppressWarnings("unchecked")
     public String getOverviewTable() {
-        //TODO: return json
-        return null;
+    	PreparedStatement st = null;
+        ResultSet rs = null;
+        JSONArray json = new JSONArray();
+        try {
+            conn = dbconn.getConnection();
+            st = conn.prepareStatement("SELECT team_id, sum(auton_top) as auton_top, sum(auton_middle) as auton_mid, sum(auton_bottom) as auton_low, sum(teleop_top) as teleop_top, sum(teleop_middle) as teleop_mid, sum(teleop_bottom) as teleop_top, sum(teleop_pyramid) as teleop_pyramid, sum(pyramid_level) as climb FROM match_record_2013 where event_id = ? GROUP BY team_id");
+            st.setString(1, "" + getSelectedEvent());
+            rs = st.executeQuery();
+            while (rs.next()) {
+            	JSONObject o = new JSONObject();
+            	int autonTotal = 6 * rs.getInt("auton_top") + 4 * rs.getInt("auton_mid") + 2 * rs.getInt("auton_low");
+            	int teleopTotal = 3 * rs.getInt("teleop_top") + 2 * rs.getInt("teleop_mid") + 1 * rs.getInt("teleop_low") + 5 * rs.getInt("teleop_pyramid");
+            	int climbTotal = rs.getInt("climb");
+            	
+            	o.put("id", rs.getInt("team_id"));
+            	o.put("autononmous", autonTotal);
+            	o.put("teleop", teleopTotal);
+            	o.put("climb", climbTotal);
+            	o.put("total_points", autonTotal + teleopTotal + climbTotal);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try{
+                conn.close();
+                st.close();
+                rs.close();
+            }catch (SQLException e) {
+                System.out.println("Error closing query");
+            }
+        }
+        return json.toString();
     }
     
     public String getOverviewChart() {
