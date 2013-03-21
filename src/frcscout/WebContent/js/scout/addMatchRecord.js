842c26801f9713a994800bb9adf9ecf1b2fbb77c
Kristian Calhoun <kristian.calhoun@gmail.com>
2013-03-21 16:56:17 -0400

    Update Edit Match Record layout to match new Add Match Record layout.

--- src/frcscout/WebContent/js/scout/addMatchRecord.js
+++ src/frcscout/WebContent/js/scout/addMatchRecord.js
@@ -1,4 +1,228 @@
 function getContentItems(){
+
+	function getGeneralMatchRecordItems() {
+		return [{
+	    	border: false,
+	    	items: [{
+	            xtype: 'combo',
+	            mode: 'local',
+	            triggerAction: 'all',
+	            forceSelection: true,
+	            editable: false,
+	            fieldLabel: 'Event Name:',
+	            name: 'eventId',
+	            displayField: 'name',
+	            valueField: 'id',
+	            queryMode: 'local',
+	            store: Ext.create('Ext.data.Store', {
+	                fields: ['id', 'name', 'location', 'start_date', 'end_date'],
+	                data: eventJSON
+	            }),
+	            allowBlank: false
+	        }, {
+	            xtype: 'textfield', //TODO should be by event / match
+	            name: 'matchId',
+	            fieldLabel: 'Match Number:',
+	            regex: /^[0-9]+$/,
+	            regexText:'Match number must be an integer',
+	            allowBlank: false
+	        }, {
+	        	layout: {
+	        		type: 'hbox',
+	        	},
+	        	border: false,
+	        	items: [{
+	                xtype: 'combo',
+	                mode: 'local',
+	                triggerAction: 'all',
+	                forceSelection: true,
+	                editable: false,
+	                fieldLabel: 'Team Number:',
+	                name: 'teamId',
+	                displayField: 'id',
+	                valueField: 'id',
+	                queryMode: 'local',
+	                store: Ext.create('Ext.data.Store', {
+	                    fields: ['id', 'name', 'location'],
+	                    data: teamJSON
+	                }),
+	                allowBlank: false
+	            }, {
+	                xtype: 'button',
+	                text: 'Add Team',
+	                handler: function() {
+	                    window.location = "/frcscout/scout/addTeam.jsp?return=scout/addMatchRecord.jsp";
+	                },
+	                margin: '0 5 0 5'
+	            }]
+	        }, {
+	            xtype: 'radiogroup',
+	            fieldLabel: 'Color',
+	            items: [{
+	                boxLabel: 'Red', name: 'color', inputValue: 'red', checked: true
+	            }, {
+	                boxLabel: 'Blue', name: 'color', inputValue: 'blue'
+	            }],
+	            width: 260
+	        }]
+	    }];
+	}
+
+	function getAutonomousMatchRecordItems() {
+		return [{
+				html: "Autonomous",
+				baseCls: 'bold',
+				border: false,
+				margin: '2 0 0 0'
+			}, {
+				html: "Enter the number of discs scored in each goal:",
+				baseCls: 'italicized',
+				border: false,
+				margin: '0 0 5 0'
+			}, {
+	            xtype: 'numberfield', 
+	            name: 'autonTop',
+	            value: 0,
+	            minValue: 0,
+	            allowBlank: false,
+	            allowDecimals: false,
+	            fieldLabel: 'Top:',
+	        }, {
+	            xtype: 'numberfield', 
+	            name: 'autonMiddle',
+	            minValue: 0,
+	            value: 0,
+	            allowBlank: false,
+	            allowDecimals: false,
+	            fieldLabel: 'Middle:',
+	        }, {
+	            xtype: 'numberfield', 
+	            name: 'autonBottom',
+	            minValue: 0,
+	            value: 0,
+	            allowBlank: false,
+	            allowDecimals: false,
+	            fieldLabel: 'Bottom:',
+	        }
+	    ];
+	}
+
+	function getTeleoperatedMatchRecordItems() {
+		return [{
+				html: "Teleoperated",
+				baseCls: 'bold',
+				border: false,
+				margin: '2 0 0 0'
+			}, {
+				html: "Enter the number of discs scored in each goal:",
+				baseCls: 'italicized',
+				border: false,
+				margin: '0 0 5 0'
+			}, {
+	            xtype: 'numberfield', 
+	            name: 'teleopTop',
+	            minValue: 0,
+	            value: 0,
+	            allowBlank: false,
+	            allowDecimals: false,
+	            fieldLabel: 'Top:',
+	        }, {
+	            xtype: 'numberfield', 
+	            name: 'teleopMiddle',
+	            minValue: 0,
+	            value: 0,
+	            allowBlank: false,
+	            allowDecimals: false,
+	            fieldLabel: 'Middle:',
+	        }, {
+	            xtype: 'numberfield', 
+	            name: 'teleopBottom',
+	            minValue: 0,
+	            value: 0,
+	            fieldLabel: 'Bottom:',
+	        },{
+	            xtype: 'numberfield', 
+	            name: 'teleopPyramid',
+	            minValue: 0,
+	            value: 0,
+	            allowBlank: false,
+	            allowDecimals: false,
+	            fieldLabel: 'Pyramid:',
+	        }
+	    ];
+	}
+
+	function getEndGameMatchRecordItems() {
+		return [{
+			html: "End Game",
+			baseCls: 'bold',
+			border: false,
+			margin: '2 0 0 0'
+		}, {
+			html: "What level of the pyramid did the robot climb to?",
+			baseCls: 'italicized',
+			border: false,
+			margin: '0 0 5 0'
+		}, {
+	        xtype: 'radiogroup',
+	        items: [{
+	            boxLabel: '0', name: 'pyramidLevel', inputValue: '0', checked: true
+	        }, {
+	            boxLabel: '1', name: 'pyramidLevel', inputValue: '1'
+	        }, {
+	            boxLabel: '2', name: 'pyramidLevel', inputValue: '2'
+	        }, {
+	            boxLabel: '3', name: 'pyramidLevel', inputValue: '3'
+	        }],
+	        width: 300
+	    }];
+	}
+
+	function getPostMatchRecordItems() {
+		return [{
+			html: "Post Match",
+			baseCls: 'bold',
+			border: false,
+			margin: '2 0 0 0'
+		}, {
+	        xtype: 'radiogroup', 
+	        fieldLabel: 'Play Style',
+	        items: [{
+	            boxLabel: 'Offensive', name: 'playStyle', inputValue: 'Offensive', checked: true
+	        }, {
+	            boxLabel: 'Defensive', name: 'playStyle', inputValue: 'Defensive'
+	        }],
+	        width: 260
+	    },{
+	        xtype: 'numberfield', 
+	        name: 'confidence',
+	        fieldLabel: 'Rating (0 - 5):',
+	        value: 0,
+	        maxValue: 5,
+	        minValue: 0
+	    },{
+	        xtype: 'numberfield', 
+	        name: 'ability',
+	        fieldLabel: 'Floor Pickup Ability (0 - 5):',
+	        value: 0,
+	        maxValue: 5,
+	        minValue: 0
+	    },{
+	        xtype: 'checkbox', 
+	        name: 'fouls',
+	        fieldLabel: 'Fouls:',
+	    },{
+	        xtype: 'checkbox', 
+	        name: 'technicalFouls',
+	        fieldLabel: 'Technical Fouls:',
+	    },{
+	        xtype: 'textarea', 
+	        name: 'comments',
+	        fieldLabel: 'Comments:',
+	        width: 300
+	    }];
+	}
+	
     return [{
         title: 'Add Match Record',
         xtype: 'form',
@@ -72,228 +296,5 @@ function getContentItems(){
                 window.location = "/frcscout/scout/scout.jsp";
             }
         }]
-    }];
-}
-
-function getGeneralMatchRecordItems() {
-	return [{
-    	border: false,
-    	items: [{
-            xtype: 'combo',
-            mode: 'local',
-            triggerAction: 'all',
-            forceSelection: true,
-            editable: false,
-            fieldLabel: 'Event Name:',
-            name: 'eventId',
-            displayField: 'name',
-            valueField: 'id',
-            queryMode: 'local',
-            store: Ext.create('Ext.data.Store', {
-                fields: ['id', 'name', 'location', 'start_date', 'end_date'],
-                data: eventJSON
-            }),
-            allowBlank: false
-        }, {
-            xtype: 'textfield', //TODO should be by event / match
-            name: 'matchId',
-            fieldLabel: 'Match Number:',
-            regex: /^[0-9]+$/,
-            regexText:'Match number must be an integer',
-            allowBlank: false
-        }, {
-        	layout: {
-        		type: 'hbox',
-        	},
-        	border: false,
-        	items: [{
-                xtype: 'combo',
-                mode: 'local',
-                triggerAction: 'all',
-                forceSelection: true,
-                editable: false,
-                fieldLabel: 'Team Number:',
-                name: 'teamId',
-                displayField: 'id',
-                valueField: 'id',
-                queryMode: 'local',
-                store: Ext.create('Ext.data.Store', {
-                    fields: ['id', 'name', 'location'],
-                    data: teamJSON
-                }),
-                allowBlank: false
-            }, {
-                xtype: 'button',
-                text: 'Add Team',
-                handler: function() {
-                    window.location = "/frcscout/scout/addTeam.jsp?return=scout/addMatchRecord.jsp";
-                },
-                margin: '0 5 0 5'
-            }]
-        }, {
-            xtype: 'radiogroup',
-            fieldLabel: 'Color',
-            items: [{
-                boxLabel: 'Red', name: 'color', inputValue: 'red', checked: true
-            }, {
-                boxLabel: 'Blue', name: 'color', inputValue: 'blue'
-            }],
-            width: 260
-        }]
-    }];
-}
-
-function getAutonomousMatchRecordItems() {
-	return [{
-			html: "Autonomous",
-			baseCls: 'bold',
-			border: false,
-			margin: '2 0 0 0'
-		}, {
-			html: "Enter the number of discs scored in each goal:",
-			baseCls: 'italicized',
-			border: false,
-			margin: '0 0 5 0'
-		}, {
-            xtype: 'numberfield', 
-            name: 'autonTop',
-            value: 0,
-            minValue: 0,
-            allowBlank: false,
-            allowDecimals: false,
-            fieldLabel: 'Top:',
-        }, {
-            xtype: 'numberfield', 
-            name: 'autonMiddle',
-            minValue: 0,
-            value: 0,
-            allowBlank: false,
-            allowDecimals: false,
-            fieldLabel: 'Middle:',
-        }, {
-            xtype: 'numberfield', 
-            name: 'autonBottom',
-            minValue: 0,
-            value: 0,
-            allowBlank: false,
-            allowDecimals: false,
-            fieldLabel: 'Bottom:',
-        }
-    ];
-}
-
-function getTeleoperatedMatchRecordItems() {
-	return [{
-			html: "Teleoperated",
-			baseCls: 'bold',
-			border: false,
-			margin: '2 0 0 0'
-		}, {
-			html: "Enter the number of discs scored in each goal:",
-			baseCls: 'italicized',
-			border: false,
-			margin: '0 0 5 0'
-		}, {
-            xtype: 'numberfield', 
-            name: 'teleopTop',
-            minValue: 0,
-            value: 0,
-            allowBlank: false,
-            allowDecimals: false,
-            fieldLabel: 'Top:',
-        }, {
-            xtype: 'numberfield', 
-            name: 'teleopMiddle',
-            minValue: 0,
-            value: 0,
-            allowBlank: false,
-            allowDecimals: false,
-            fieldLabel: 'Middle:',
-        }, {
-            xtype: 'numberfield', 
-            name: 'teleopBottom',
-            minValue: 0,
-            value: 0,
-            fieldLabel: 'Bottom:',
-        },{
-            xtype: 'numberfield', 
-            name: 'teleopPyramid',
-            minValue: 0,
-            value: 0,
-            allowBlank: false,
-            allowDecimals: false,
-            fieldLabel: 'Pyramid:',
-        }
-    ];
-}
-
-function getEndGameMatchRecordItems() {
-	return [{
-		html: "End Game",
-		baseCls: 'bold',
-		border: false,
-		margin: '2 0 0 0'
-	}, {
-		html: "What level of the pyramid did the robot climb to?",
-		baseCls: 'italicized',
-		border: false,
-		margin: '0 0 5 0'
-	}, {
-        xtype: 'radiogroup',
-        items: [{
-            boxLabel: '0', name: 'pyramidLevel', inputValue: '0', checked: true
-        }, {
-            boxLabel: '1', name: 'pyramidLevel', inputValue: '1'
-        }, {
-            boxLabel: '2', name: 'pyramidLevel', inputValue: '2'
-        }, {
-            boxLabel: '3', name: 'pyramidLevel', inputValue: '3'
-        }],
-        width: 300
-    }];
-}
-
-function getPostMatchRecordItems() {
-	return [{
-		html: "Post Match",
-		baseCls: 'bold',
-		border: false,
-		margin: '2 0 0 0'
-	}, {
-        xtype: 'radiogroup', 
-        fieldLabel: 'Play Style',
-        items: [{
-            boxLabel: 'Offensive', name: 'playStyle', inputValue: 'Offensive', checked: true
-        }, {
-            boxLabel: 'Defensive', name: 'playStyle', inputValue: 'Defensive'
-        }],
-        width: 260
-    },{
-        xtype: 'numberfield', 
-        name: 'confidence',
-        fieldLabel: 'Rating (0 - 5):',
-        value: 0,
-        maxValue: 5,
-        minValue: 0
-    },{
-        xtype: 'numberfield', 
-        name: 'ability',
-        fieldLabel: 'Floor Pickup Ability (0 - 5):',
-        value: 0,
-        maxValue: 5,
-        minValue: 0
-    },{
-        xtype: 'checkbox', 
-        name: 'fouls',
-        fieldLabel: 'Fouls:',
-    },{
-        xtype: 'checkbox', 
-        name: 'technicalFouls',
-        fieldLabel: 'Technical Fouls:',
-    },{
-        xtype: 'textarea', 
-        name: 'comments',
-        fieldLabel: 'Comments:',
-        width: 300
     }];
 }
